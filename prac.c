493e686495ab5af0c10b88d6a3a70a395adf64f1
AbhineetGarga <coderboiiye@gmail.com>
2024-02-10 21:20:50 +0530

    first commit

---
+++ prac.c
@@ -0,0 +1,20 @@
+ #include<stdio.h>
+ void main()
+ {
+ 	int num1, num2,sum, sub, mod;
+// 	declearation of variable 
+    scanf("%d %d", &num1, &num2);
+//    input the operation 
+    sum= num1 + num2;
+//	addition of the number and sturing sum 
+    printf("\nThe Sum Is= %d", sum);
+//	subtraction of number
+    sub= num1 - num2;
+//  addition of the number and sturing sum  
+    printf("\nTher difference is=%d", sub);
+//  mod of the number 
+    mod= num1%num2;
+//	modulus of number 
+    printf("\nThe modulus is=%d", mod);
+//	display the output  
+ }
